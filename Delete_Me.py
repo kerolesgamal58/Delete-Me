5044d5327880fd18823813a1154e74fec901b6a6
kerolesgamal58 <63745141+kerolesgamal58@users.noreply.github.com>
2022-05-02 16:48:43 +0200

    Update Delete_Me.py

--- Delete_Me.py
+++ Delete_Me.py
@@ -1,3 +1,10 @@
+# This tool is used to delete any file from disk and make it impossible to recover it again
+# First we open the file and write on it n bytes of null byte 0x00
+# and rename the file (using random alphanumeric characters) to delete any artifact about filename
+
+# note: we could recover the filename from $I30 index entry on ntfs
+# this entry exists in every directory and contains index of all files/directories that belong to this directory
+
 import os
 import random
 import string
@@ -20,6 +27,7 @@ else:
     OldFileNameLen = len(OldFileName.rsplit('.', 1)[0])
     OldExtensionLen = len(OldFileName.rsplit('.', 1)[-1])
 
+    # Generate a new name with random alphanumeric characters with the same original length
     NewFileName = ''.join(random.choices(string.ascii_letters +  string.digits, k=OldFileNameLen))
     NewExtension = ''.join(random.choices(string.ascii_letters +  string.digits, k=OldExtensionLen))
     NewFilePath = OldFilePath.rsplit('\\', 1)[0] + "\\" + NewFileName + "." + NewExtension
